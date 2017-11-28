873c55422a76cfcf0ff4ad052d6e8fe445ec409c
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-28 22:25:26 +1100

    Add onMessageChange function

--- src/App.js
+++ src/App.js
@@ -8,18 +8,33 @@ import ConvertToNato from './lib/nato'
 
 class App extends Component {
   state = {
-    phonetic: 'Hello'
+    message: ''
   }
+
+  onMessageChange = (message) => {
+    this.setState( (prevState) => {
+      const beforeItems = prevState.message
+      // const afterItems = message
+      return {
+        message: message
+      }
+    })
+  }
+
   render() {
-    const phonetic = ConvertToNato(this.state.phonetic);
+    const phonetic = ConvertToNato(this.state.message);
 
     return (
       <div className="App">
         <div className="container">
-          <Label htmlFor='message' title='Your Message' />
-          <TextArea id='message' rows='5' placeholder='Enter your message' />
-          <Label htmlFor='nato' title='NATO Phonetic' />
-          <TextArea id='nato' rows='5' placeholder='Enter your message' value={phonetic}/>
+          <strong><Label htmlFor='message' title='Your Secret Message' /></strong>
+          <TextArea id='message' rows='5' placeholder='Enter your message'
+            onTextChange={ (message) => {
+              this.onMessageChange(message)
+            }} />
+          <br/>
+          <strong><Label htmlFor='nato' title='NATO Phonetic' /></strong>
+          <TextArea id='nato' rows='5' placeholder='' value={phonetic} readOnly />
         </div>
       </div>
     );
