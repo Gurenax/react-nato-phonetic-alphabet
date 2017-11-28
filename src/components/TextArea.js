f5479aaac07542936c130b381538a811411216a4
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2017-11-28 22:21:28 +1100

    Add onChange and readOnly property

--- src/components/TextArea.js
+++ src/components/TextArea.js
@@ -6,8 +6,18 @@ import React from 'react'
 *  rows - number of rows
 *  placeholder - placeholder description
 */
-const TextArea = ({ id, rows='3', placeholder, value }) => ( 
-  <textarea className="form-control" id={ id } rows={ rows } placeholder={ placeholder } value={ value } />
+const TextArea = ({ id, rows='3', placeholder, value, readOnly=false, onTextChange }) => ( 
+  <textarea
+    className="form-control"
+    id={ id }
+    rows={ rows }
+    placeholder={ placeholder }
+    value={ value }
+    readOnly={ readOnly }
+    onChange={ (event) => {
+      // if onTextChange attribute is present, perform event
+      onTextChange && onTextChange(event.target.value)
+    }}/>
 )
 
 export default TextArea
